9618b40104e85f418241474b4bfe2badc2c5a3b8
Sarah Kaydence <sarah.kydnc@gmail.com>
2022-07-01 15:23:35 +0800

    Integrated Bootstrap to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        Bright Suns / Rising Moons,
-        <br />
-        Galactic Traveller!
-      </h1>
-      <h3>What is it you are looking for today?</h3>
+      <div className="container p-5">
+        <h1>
+          Bright Suns / Rising Moons,
+          <br />
+          Galactic Traveller!
+        </h1>
+        <h3>What is it you are looking for today?</h3>
+      </div>
     </div>
   );
 }
