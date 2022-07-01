805c1a0819338289c8d2d915a890d752a4fe39b3
Sarah Kaydence <sarah.kydnc@gmail.com>
2022-07-01 16:29:41 +0800

    Added and simple-styled Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import Datapad from "./Components/Datapad";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container p-5">
+    <div className="App mt-5">
+      <div className="container py-5 shadow">
         <h1>
           Bright Suns / Rising Moons,
           <br />
           Galactic Traveller!
         </h1>
+
         <h3>What is it you are looking for today?</h3>
+
+        <Datapad />
       </div>
     </div>
   );
---
+++ src/Components/Datapad.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Datapad.css";
+
+function Datapad() {
+  return (
+    <div>
+      <form id="find-subject-form" className="mt-5 px-5">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your query"
+            aria-label="Enter your query"
+            aria-describedby="button-addon2"
+          />
+          <div className="input-group-append">
+            <button className="btn" type="button" id="button-addon2">
+              Button
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Datapad;
