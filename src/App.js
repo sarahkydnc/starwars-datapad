e2a137a9fb790a23e111d77883281cefb7bdd888
Sarah Kaydence <sarah.kydnc@gmail.com>
2022-07-01 12:03:10 +0800

    Added custom fonts and tester CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Bright Suns / Rising Moons,
+        <br />
+        Galactic Traveller!
+      </h1>
+      <h3>What is it you are looking for today?</h3>
+    </div>
+  );
 }
 
 export default App;
